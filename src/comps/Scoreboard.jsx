c61f073a81a7a137b3f2479cd888708a00e43ded
Paul Abib Camano <paul.camano@elinnovtech.com>
2023-08-08 23:25:02 +0800

    Update empty scoreboard message

--- src/comps/Scoreboard.jsx
+++ src/comps/Scoreboard.jsx
@@ -1,5 +1,5 @@
 import Team from "./Team"
 
 export default function Scoreboard({ players, updateScore, removePlayer, updatePlayerName }){
-    return players && players.length > 0 ? players.map((player) => <Team key={player.id} team={player} updateScore={updateScore} removePlayer={removePlayer} updatePlayerName={updatePlayerName} />) : <p className="text-center text-xl">No players yet. Add a player.</p>
+    return players && players.length > 0 ? players.map((player) => <Team key={player.id} team={player} updateScore={updateScore} removePlayer={removePlayer} updatePlayerName={updatePlayerName} />) : <p className="text-center text-xl">No players yet. Click + to add a player.</p>
 }
