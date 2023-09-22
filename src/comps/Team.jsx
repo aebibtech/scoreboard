42cc3779e01bd5c0bd09c67db051ef73978d883d
Paul Abib S. Camano <paul.camano@gmail.com>
2023-09-22 15:50:05 +0000

    Fix player list responsiveness

--- src/comps/Team.jsx
+++ src/comps/Team.jsx
@@ -28,7 +28,7 @@ export default function Team({team, updateScore, removePlayer, updatePlayerName}
         }
     }
     
-    const liStyles = "py-6 text-4xl h-auto"
+    const liStyles = "py-6 text-4xl h-auto flex justify-between flex-wrap"
     const nameStyles = "inline-block w-[300px] leading-relaxed font-semibold overflow-hidden bg-transparent"
     const scoreButtonStyles = "border border-black px-3 hover:bg-black hover:text-white mr-3"
     const deleteButtonStyles = "border border-red-900 bg-red-900 px-3 hover:opacity-80"
@@ -37,10 +37,12 @@ export default function Team({team, updateScore, removePlayer, updatePlayerName}
         <li className={liStyles}>
             <input type="text" ref={nameRef} className={nameStyles} value={name} onChange={handleNameChange} onKeyUp={handleKey} />
             {/* <span className={nameStyles}>{team ? team.name : ""}</span> */}
-            <span className="inline-block w-[72px] ml-3 text-center mr-6 bg-transparent">{score}</span>
-            <button className={scoreButtonStyles} onClick={ () => setScore(() => score + 1) }>+</button> 
-            <button className={scoreButtonStyles} onClick={ () => setScore(() => score > 1 ? score - 1 : 0) }>-</button>
-            <button className={deleteButtonStyles} onClick={() => { removePlayer(team.id) }}>&#128465;</button>
+            <span>
+                <span className="inline-block w-[72px] ml-3 text-center mr-6 bg-transparent">{score}</span>
+                <button className={scoreButtonStyles} onClick={ () => setScore(() => score + 1) }>+</button> 
+                <button className={scoreButtonStyles} onClick={ () => setScore(() => score > 1 ? score - 1 : 0) }>-</button>
+                <button className={deleteButtonStyles} onClick={() => { removePlayer(team.id) }}>&#128465;</button>
+            </span>
         </li>
     )
 }
